bc1527e5c31ffaddd8ddd3e24d61a6029e2080a8
javi77pineda <jpineda0000@gmail.com>
2017-11-27 15:27:55 -0500

    Add files via upload

---
+++ turismocolon WEB/contacto.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Two Oceans Tours</title>
+<link href="css/main.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+body {
+	background-image: url(imagenes/wallpaper6.jpg);
+}
+</style>
+</head>
+
+<body>
+<div id="container">
+<div class="header"><h1 align="center">
+  <div align="center"><img src="imagenes/LOGO.png" width="328" height="173" alt=""/></div>
+</div>
+<div class="header2"><h1 align="center"><marquee behavior="scroll" direction="left" scrollamount="5">
+<br>
+<em>Two Oceans Tours Panamá S.A.</em>
+</marquee></h1></div>
+<div id="menu">
+    <ul>
+    <li>
+        <h3><a href="index.html" class="ul">Inicio</a></h3>
+      </li>
+      <li>
+        <h3><a href="girasturisticas.html" class="ul">Giras turisticas</a></h3>
+      </li>
+      <li>
+        <h3><a href="puertodecrucero.html">Puertos de crucero</a></h3>
+      </li>
+      <li>
+        <h3><a href="galeria.html">Galeria</a></h3>
+      </li>
+      <li>
+        <h3><a href="recomendaciones.html">Recomendaciones</a></h3>
+      </li>
+      <li>
+        <h3><a href="contacto.html">Contacto</a></h3>
+      </li>
+    </ul>
+</div>
+        
+<div id="contact1"></div>
+
+<form method="post" action="http://parking507.com/modules/mod_js_social_tabs_slider/tmpl/form/contact.php" name="contactform" id="contactform">
+
+			<fieldset>
+
+			<legend>Llene los espacios en blanco.</legend>
+
+			<label for="name" accesskey="U"><span class="required">*</span>Name</label>
+			<input name="name" type="text" id="name" size="50" value="" />
+
+			<br />
+			<label for="email" accesskey="E"><span class="required">*</span> E-mail</label>
+			
+			<input name="email" type="text" id="email" size="49" value="" />
+
+			<br />
+			<label for="subject" accesskey="S"><span class="required">*</span>Tema</label>
+			<input name="subject" type="text" id="subject" size="51" value="" />
+			
+			<br />
+			<label for="comments" accesskey="C"><span class="required">*</span>Comentarios</label>
+			<textarea name="comments" cols="40" rows="3" id="comments" style="width: 270px;"></textarea>
+
+			<p><span class="required">*</span> Eres humano?</p>
+
+			<label for="verify" accesskey="V">&nbsp;&nbsp;&nbsp;4 - 3 =</label>
+			<input name="verify" type="text" id="verify" size="4" value="" style="width: 30px;" /><br /><br />
+
+			<input type="submit" class="submit" id="submit" value="Enviar" />
+
+			</fieldset>
+
+			</form>
+        
+<div id="bocasdeltoroindex"></div>      
+      
+  <div id="info">Para mas información contactanos al correo: twooceanstours@hotmail.com</div>
+          <div id="footer">Copyright © 2014 Two Oceans tours. Desarrollado por Branko Seferlis.</div>
+        
+</div>
+</body>
+</html>
